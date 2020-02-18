4b1de0476edd78e5151e5f20881d719b1a330fea
maarkeez <d.marquez.delgado.91@gmail.com>
2020-02-18 22:00:31 +0100

    Initial Commit

---
+++ photos-loader-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  photos-loader-client
+//
+//  Created by David Márquez Delgado on 18/02/2020.
+//  Copyright © 2020 David Márquez Delgado. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
